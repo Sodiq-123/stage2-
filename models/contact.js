d6ef99587bb0951376b8f9fe753014372dacb78c
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2021-08-18 22:26:40 +0100

    Added contact Schema

---
+++ models/contact.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var contactSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+});
